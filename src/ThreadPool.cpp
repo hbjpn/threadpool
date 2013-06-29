ce03ae73d178c7261adc078dca2d2f678c73feed
hbjpn <tokyochofu@gmail.com>
2013-06-30 00:50:20 +0900

    Add initial version

---
+++ src/ThreadPool.cpp
@@ -0,0 +1,95 @@
+/**
+*
+* Copyright (C) 2012-2013 Hiroyuki Baba, All Rights Reserved.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License or any
+* later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR POURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#include "ThreadPool.h"
+
+class PseudoTerminator : public Job
+{
+public:
+    PseudoTerminator(){}
+    virtual ~PseudoTerminator(){}
+    virtual void run(){ abort(); }
+};
+
+void ThreadPool::run()
+{
+    while(1)
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        while(!(_runTh < _numTh && _jobs.size() > 0))
+            _cv.wait(lock);
+        
+        if( typeid(*_jobs.begin()->get()) == typeid(PseudoTerminator) ) break;
+    
+        std::thread([this](JobPtr job){ job->run(); notifyJobFinish(); }, *_jobs.begin()).detach();
+        _jobs.erase(_jobs.begin());
+        ++_runTh;
+    }
+    
+    /* Wait until all the executing jobs end */
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        while( _runTh > 0 )
+            _cv.wait(lock);
+    }
+}
+
+void ThreadPool::notifyJobFinish()
+{
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        --_runTh;
+    }
+    _cv.notify_one();
+}
+
+ThreadPool::ThreadPool(int numThread)
+    : _th(NULL), _numTh(numThread), _runTh(0)
+{
+}
+
+void ThreadPool::join()
+{
+    if(_th)
+    {
+        add(JobPtr(new PseudoTerminator()));
+        _th->join();
+        delete _th;
+        _th = NULL;
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    join();
+}
+
+void ThreadPool::start()
+{
+    _th = new std::thread([this](void){ run(); });
+}
+
+void ThreadPool::add(JobPtr job)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    _jobs.push_back(job);
+    _cv.notify_one();
+}
+
+
---
+++ src/ThreadPool.h
@@ -0,0 +1,100 @@
+/**
+*
+* Copyright (C) 2012-2013 Hiroyuki Baba, All Rights Reserved.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License or any
+* later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR POURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include <list>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <memory>
+
+/**
+ * Job class
+ */
+class Job
+{
+public:
+    Job(){}
+    virtual ~Job(){}
+
+    virtual void run() = 0;
+};
+
+typedef std::shared_ptr<Job> JobPtr;
+
+/**
+ * Thread pool class
+ */
+class ThreadPool
+{
+    std::list<JobPtr> _jobs;
+    std::mutex _mutex;
+    std::thread* _th;
+    std::condition_variable _cv;
+
+    int _numTh;
+    int _runTh;
+public:
+    /**
+     * Constructor
+     *
+     * @param numThread Number of available thread
+     */
+    ThreadPool(int numThread);
+    
+    /**
+     * Destructor
+     * 
+     * Wait for end of execution of currently queued jobs if any.
+     */
+    virtual ~ThreadPool();
+    
+    /**
+     * Main loop of job pooling thread
+     */
+    void run();
+    
+    /**
+     * Notify end of single job
+     */
+    void notifyJobFinish();
+    
+    /**
+     * Add job for the queue
+     *
+     * @param job Job to add
+     */
+    void add(JobPtr job);
+    
+    /**
+     * Start executing jobs
+     */
+    void start();
+    
+    /**
+     * Wait for end of execution of currently queued jobs
+     */
+    void join(); 
+};
+
+
+
+#endif
